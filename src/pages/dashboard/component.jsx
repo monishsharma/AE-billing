5184a265a6e7beb3131357646fb35c886194f3a8
monishsharma <sharma.monish17@gmail.com>
2025-05-05 16:32:04 +0530

    removed mui chars and used chartjs

--- src/pages/dashboard/component.jsx
+++ src/pages/dashboard/component.jsx
@@ -11,37 +11,141 @@ import DatePicker from "react-datepicker";
 import { Col, Row } from "react-bootstrap";
 import { COMPANY_TYPE } from "../../constants/app-constant";
 import PageLoader from "../../components/page-loader";
-import { BarChart } from '@mui/x-charts/BarChart';
-import { PieChart } from "@mui/x-charts";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
+import { Bar, Pie } from "react-chartjs-2";
 
-const Dashboard = ({
-  getReportConnect
-}) => {
-
+const Dashboard = ({ getReportConnect }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [value, setValue] = useState(COMPANY_TYPE.ASHOK);
   const [dateValue, setDateValue] = useState(new Date());
   const [reportStat, setReportStat] = useState({});
   const monthlyData = reportStat?.monthlyTotals || Array(12).fill(0);
-  const config = {
-    height: 350,
-    margin: { left: 40 },
-    hideLegend: true,
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement
+  );
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Sales Per Month",
+      },
+    },
+  };
+
+  const pieOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Customer Breakdown",
+      },
+    },
+  };
+
+  const labels = [
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+    "Jan",
+    "Feb",
+    "Mar",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "₹ Sales",
+        data: monthlyData,
+        backgroundColor: "red",
+      },
+    ],
+  };
+
+  const getLabel = () => {
+    let label = [];
+    let amount = [];
+    if (reportStat && reportStat.customerTotals) {
+      Object.keys(reportStat.customerTotals).map((key) => label.push(key));
+      Object.keys(reportStat.customerTotals).map((key) => amount.push(reportStat.customerTotals[key].total));
+    }
+
+    return {label, amount};
+  };
+
+  const pieData = {
+    labels: getLabel().label,
+    datasets: [
+      {
+        label: "Sales to company",
+        data: getLabel().amount,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
   };
 
   useEffect(() => {
     setIsLoading(true);
-    getReportConnect(({company:value,  month: dateValue.getMonth() +1 , year: dateValue.getFullYear()}))
-    .then((res) => {
-      setReportStat(res);
-      setIsLoading(false);
+    getReportConnect({
+      company: value,
+      month: dateValue.getMonth() + 1,
+      year: dateValue.getFullYear(),
     })
-    .catch(() => {
-      setIsLoading(false);
-      setReportStat({});
-    })
-  }, [value, dateValue])
-
+      .then((res) => {
+        setReportStat(res);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setIsLoading(false);
+        setReportStat({});
+      });
+  }, [value, dateValue]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -55,130 +159,87 @@ const Dashboard = ({
     </div>
   ));
 
-
   const handleDateChange = (selectedDate) => {
-    setDateValue(selectedDate)
-}
-  console.log(reportStat?.monthlyTotals)
+    setDateValue(selectedDate);
+  };
+  console.log(reportStat?.monthlyTotals);
   const renderReport = () => {
     return (
       <>
         <div className="d-flex align-items-center w-100">
-              <Row className="w-100">
-                <Col sm={2}>
-                  <div className="mb-4 d-flex justify-content-start mt-4">
-                    <DatePicker
-                      selected={dateValue}
-                      wrapperClassName="w-100"
-                      showMonthYearPicker
-                      onChange={handleDateChange}
-                      dateFormat="MMMM, YYYY"
-                      customInput={<ExampleCustomInput />}
-                    />
-                  </div>
-                </Col>
-              </Row>
-            </div>
-            <Grid container spacing={2}>
-              <Grid item size={{ md: 3, xs: 12 }}>
-                <Card
-                  symbol={true}
-                  count={reportStat?.totalInvoiceAmount}
-                  title={"Total Sales"}
-                  CardIcon={CurrencyRupeeIcon}
-                  percentage={reportStat?.invoiceAmountChange?.percentage}
-                  growth={reportStat?.invoiceAmountChange?.growth}
+          <Row className="w-100">
+            <Col sm={2}>
+              <div className="mb-4 d-flex justify-content-start mt-4">
+                <DatePicker
+                  selected={dateValue}
+                  wrapperClassName="w-100"
+                  showMonthYearPicker
+                  onChange={handleDateChange}
+                  dateFormat="MMMM, YYYY"
+                  customInput={<ExampleCustomInput />}
                 />
-              </Grid>
-              <Grid item size={{ md: 3, xs: 12 }}>
-                <Card
-                  count={reportStat?.totalItems}
-                  title={"Total Invoices"}
-                  CardIcon={ReceiptIcon}
-                  percentage={reportStat?.invoiceCountChange?.percentage}
-                  growth={reportStat?.invoiceCountChange?.growth}
-                />
-              </Grid>
-              <Grid item size={{ md: 3, xs: 12 }}>
-                <Card
-                  symbol={true}
-                  count={reportStat?.unpaidTotal}
-                  title={"Total Unpaid Amount"}
-                  CardIcon={CurrencyRupeeIcon}
-                  // percentage={"16.9"}
-                  // growth={true}
-                  showPercentage={false}
-                />
-              </Grid>
-              <Grid item size={{ md: 3, xs: 12 }}>
-                <Card
-                  symbol={true}
-                  count={reportStat?.paidTotal}
-                  title={"Total Paid Amount"}
-                  CardIcon={CurrencyRupeeIcon}
-                  // percentage={"16.9"}
-                  // growth={true}
-                  showPercentage={false}
-
-                />
-              </Grid>
-              <Grid size={{md: 12}} mt={4}>
-                <Paper variant="elevation" elevation={4} square={false}>
-                    <Typography variant="h6" padding={1}>
-                      Sales per month
-                    </Typography>
-                    <BarChart
-                     {...config}
-                        xAxis={[{
-                          data: ['Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar'],
-                          scaleType: 'band',
-                          tickLabelInterval: 1,
-                          label: 'Month',
-                        }]}
-                        yAxis={[{
-                          min: 0,
-                          max: 4000000,
-                          colorMap: {
-                            type: 'piecewise',
-                            thresholds: [0],
-                            colors: value=== COMPANY_TYPE.ASHOK ? ['orange'] : ["yellow"],
-                          },
-                          tickMinStep: 1000000,
-                          label: 'Amount (₹)',
-                        }]}
-                        series={[
-                          { data: monthlyData }
-                        ]}
-                        height={400}
-                  />
-                  </Paper>
-              </Grid>
-              {/* <Grid size={{md: 6}} mt={4}>
-                <Paper variant="elevation" elevation={4} square={false}>
-                <Typography variant="h6" padding={1}>
-                      Sales per month
-                    </Typography>
-                    <PieChart
-                      series={[
-                        {
-                          data: [
-                            { id: 0, value: reportStat?.unpaidTotal, label: 'Unpaid' },
-                            { id: 1, value: reportStat?.paidTotal, label: 'Paid' },
-                          ],
-                        },
-                      ]}
-                      width={200}
-                      height={200}
-                    />
-                  </Paper>
-              </Grid> */}
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <Grid container spacing={2}>
+          <Grid item size={{ md: 3, xs: 12 }}>
+            <Card
+              symbol={true}
+              count={reportStat?.totalInvoiceAmount}
+              title={"Total Sales"}
+              CardIcon={CurrencyRupeeIcon}
+              percentage={reportStat?.invoiceAmountChange?.percentage}
+              growth={reportStat?.invoiceAmountChange?.growth}
+            />
+          </Grid>
+          <Grid item size={{ md: 3, xs: 12 }}>
+            <Card
+              count={reportStat?.totalItems}
+              title={"Total Invoices"}
+              CardIcon={ReceiptIcon}
+              percentage={reportStat?.invoiceCountChange?.percentage}
+              growth={reportStat?.invoiceCountChange?.growth}
+            />
+          </Grid>
+          <Grid item size={{ md: 3, xs: 12 }}>
+            <Card
+              symbol={true}
+              count={reportStat?.unpaidTotal}
+              title={"Total Unpaid Amount"}
+              CardIcon={CurrencyRupeeIcon}
+              // percentage={"16.9"}
+              // growth={true}
+              showPercentage={false}
+            />
+          </Grid>
+          <Grid item size={{ md: 3, xs: 12 }}>
+            <Card
+              symbol={true}
+              count={reportStat?.paidTotal}
+              title={"Total Paid Amount"}
+              CardIcon={CurrencyRupeeIcon}
+              // percentage={"16.9"}
+              // growth={true}
+              showPercentage={false}
+            />
+          </Grid>
+          <Grid container size={{ md: 12 }} mt={4}>
+            <Grid item size={{ md: 6 }}>
+              <Bar key={value + dateValue} options={options} data={data} />;
             </Grid>
+            <Grid item size={{ md: 6 }}>
+              <Box sx={{ maxWidth: 400, margin: "auto" }}>
+                <Pie data={pieData} options={pieOptions} />
+              </Box>
+            </Grid>
+          </Grid>
+        </Grid>
       </>
+    );
+  };
 
-    )
-  }
-
-  if (isLoading) return <PageLoader />
+  if (isLoading) return <PageLoader />;
 
   return (
     <>
@@ -193,22 +254,28 @@ const Dashboard = ({
               <Tab label="Padma" value={COMPANY_TYPE.PADMA} />
             </TabList>
           </Box>
-          <TabPanel sx={{
-            p:{
-              xs: 0,
-              sm: 2,
-              md: 0
-            }
-          }} value={COMPANY_TYPE.ASHOK}>
+          <TabPanel
+            sx={{
+              p: {
+                xs: 0,
+                sm: 2,
+                md: 0,
+              },
+            }}
+            value={COMPANY_TYPE.ASHOK}
+          >
             {renderReport()}
           </TabPanel>
-          <TabPanel sx={{
-            p:{
-              xs: 0,
-              sm: 2,
-              md: 0
-            }
-          }} value={COMPANY_TYPE.PADMA}>
+          <TabPanel
+            sx={{
+              p: {
+                xs: 0,
+                sm: 2,
+                md: 0,
+              },
+            }}
+            value={COMPANY_TYPE.PADMA}
+          >
             {renderReport()}
           </TabPanel>
         </TabContext>
