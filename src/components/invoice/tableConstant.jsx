98f6c0edc927ca868038c84251088cbd8c6f37ae
Monish sharma <sharma.monish17@gmail.com>
2025-08-13 11:14:06 +0530

    Update tableConstant.jsx

--- src/components/invoice/tableConstant.jsx
+++ src/components/invoice/tableConstant.jsx
@@ -94,7 +94,7 @@ export const tableConstants = ({
             field: 'date',
             headerName: 'Date',
             style: {
-                minWidth: 125,
+                minWidth: 150,
             },
             render: (rowData) => `${moment(rowData.invoiceDetail.invoiceDate).format("LL")}`,
         },
