852287a4eb70200cb61dbef55a08e6c0d64ad75f
monish <sharma.monish17@gmail.com>
2026-06-11 16:56:34 +0530

    Added bearer token for authorsing api routes

--- src/services/baseApi.js
+++ src/services/baseApi.js
@@ -1,21 +1,30 @@
 import axios from "axios";
+import { getAuth } from "firebase/auth";
 //
 // /* Create axios instance */
 const api = axios.create({
-    // baseURL: "http://localhost:5050",
-    baseURL: "https://ashok-enterprises-api.vercel.app",
+    baseURL: "http://localhost:5050",
+    // baseURL: "https://ashok-enterprises-api.vercel.app",
     headers: {
         "Content-Type": "application/json",
-        'Access-Control-Allow-Origin': '*',
         "X-Application-Type": "React" // Added for php api to identify requesting application
     }
 });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use(
+  async (config) => {
+    const auth = getAuth();
+
+    if (auth.currentUser) {
+      const token = await auth.currentUser.getIdToken();
+
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
     return config;
-}, (error) => {
-    return Promise.reject(error);
-});
+  },
+  (error) => Promise.reject(error)
+);
 
 /**
  * Passes response.data to services.
